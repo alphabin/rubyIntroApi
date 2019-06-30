8b8e2064370d51279f45c79dcce3fcb016a51acf
EC2 Default User <ec2-user@ip-172-31-32-181.us-east-2.compute.internal>
2019-06-30 02:38:21 +0000

    update with name printed

--- demo.rb
+++ demo.rb
@@ -1,11 +1,17 @@
 class Hello 
-  
-  def initialize name 
+ #you can use paranthesis or not 
+  def initialize (name)
     @name = name
+    #every method returns a value the last val
   end 
   
   def sayHello
+    #@sign is instance variable
    "hello "+@name
   end 
   
 end 
+
+
+h = Hello.new "Chatura" #builds new object
+puts h.sayHello #prints out
